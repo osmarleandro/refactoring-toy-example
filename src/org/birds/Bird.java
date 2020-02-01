7389e173cd259b57013edee4329e3251a59db083
Osmar Leandro <osmarleandrods@gmail.com>
2020-02-01 19:38:24 -0300

    ref: Pull up method Duck:quack() to Bird:quack()

--- src/org/birds/Bird.java
+++ src/org/birds/Bird.java
@@ -6,4 +6,7 @@ public class Bird {
 		
 	}
 
+    public void quack() {
+        System.out.println("quack");
+    }
 }
--- src/org/birds/Duck.java
+++ src/org/birds/Duck.java
@@ -2,8 +2,4 @@ package org.birds;
 
 public class Duck extends Bird {
 
-	public void quack() {
-		System.out.println("quack");
-	}
-
 }

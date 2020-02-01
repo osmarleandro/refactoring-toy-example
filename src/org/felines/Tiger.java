e54b8ac13b8cee99577b15f98dfe3d3f3db665de
Osmar Leandro <osmarleandrods@gmail.com>
2020-02-01 19:25:57 -0300

    ref: Create and encapsulate field Tiger#force

--- src/org/felines/Tiger.java
+++ src/org/felines/Tiger.java
@@ -5,12 +5,19 @@ package org.felines;
 public class Tiger extends Feline implements Animal{
 
 	private int speed;
+	private long force;
 
 	public int getSpeed() {
 		return speed;
 	}
 	public void setSpeed(int speed) {
 		this.speed = speed;
+	}
+	private long getForce() {
+		return force;
+	}
+	private void setForce(long force) {
+		this.force = force;
 	}
 	
 	

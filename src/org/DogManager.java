8385a54fa23573a9159750155688f5f1c7fdd39c
Osmar Leandro <osmarleandrods@gmail.com>
2020-02-06 09:28:47 -0300

    ref: Move method at DogManager::doStuff() to Dog::doStuff()

--- src/org/DogManager.java
+++ src/org/DogManager.java
@@ -8,19 +8,6 @@ public class DogManager {
 	public DogManager(Dog aDog) {
 		this.dog = aDog;
 	}
-	
-	public void doStuff() {
-		barkBark(this.dog);
-		
-		int age = dog.getAge();
-		int sum = 0;
-		for (int i = 0; i < age; i++) {
-			System.out.println(i);
-			sum += i;
-		}
-		sum -= dog.magicNumber;
-		System.out.println("...");
-	}
 
 	private void renameMethod() {
 		System.out.println("ruff");
--- src/org/animals/Dog.java
+++ src/org/animals/Dog.java
@@ -1,6 +1,8 @@
 package org.animals;
 
 
+import org.DogManager;
+
 public class Dog {
 
 	private int age = 0;
@@ -10,4 +12,16 @@ public class Dog {
 		return this.age;
 	}
 
+	public void doStuff(DogManager dogManager) {
+		barkBark(this);
+
+		int age = getAge();
+		int sum = 0;
+		for (int i = 0; i < age; i++) {
+			System.out.println(i);
+			sum += i;
+		}
+		sum -= magicNumber;
+		System.out.println("...");
+	}
 }
